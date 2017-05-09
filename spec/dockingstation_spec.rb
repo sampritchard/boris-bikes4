cb9648f46939d77053785879b15451839c6bd660
jade <jadealvares@hotmail.co.uk>
2017-05-09 15:33:24 +0100

    updated spec/dockingstation_spec.rb

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,6 +2,6 @@ require "dockingstation.rb"
 
 describe DockingStation do
   it "should be able to responde to release_bike" do
-    expect(subject).to respond_to release_bike
+    expect(subject).to respond_to :release_bike
   end
 end
