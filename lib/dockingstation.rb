ee6b184112afce2ee633e8a608e30e4efeb4d996
Sam Pritchard <sampritchard@msn.com>
2017-05-11 15:36:40 +0100

    15 - refactor and private methods added

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,21 +1,28 @@
 require_relative 'bike'
 
-class DockingStation
-attr_reader :bike
+  class DockingStation
+    attr_reader :bike
 
-def initialize
-  @bikes = []
-end
+    def initialize
+      @bikes = []
+    end
 
-  def release_bike
-    fail 'No bikes available' if @bikes.empty?
-    @bikes.pop
-  end
+    def release_bike
+      fail 'No bikes available' if empty?
+      @bikes.pop
+    end
 
-  def dock_bike(bike)
-    fail 'Docking station full' if @bikes.count >= 20
-    @bikes << bike
-  end
+    def dock_bike(bike)
+      fail 'Docking station full' if full? #self
+      @bikes << bike
+    end
 
+    private
+      def full?
+        @bikes.count >= 20
+      end
 
+      def empty?
+        @bikes.count == 0
+      end
 end
