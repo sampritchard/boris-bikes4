84c8a90d7a837ea218a75cd95d0bc97806668cd6
Kynosaur <kye.bracey@gmail.com>
2017-05-12 16:54:12 +0100

    Update dockingstation_spec.rb to isolate non-isolated tests with doubles (tests currently fail). Also added reminder to fix two broken tests which refer to @bikes outside of the class

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -3,7 +3,7 @@ require "dockingstation.rb"
 describe DockingStation do
   describe '#release_bike' do
     it 'releases a bike' do
-      bike = Bike.new
+      bike = double(:bike)
       subject.dock_bike(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -14,27 +14,27 @@ end
 
   describe '#dock_bike' do
     it 'raises an error when attempting to dock a bike in a full station' do
-      subject.capacity.times { subject.dock_bike Bike.new }
-      expect { subject.dock_bike Bike.new }.to raise_error 'Docking station full'
+      subject.capacity.times { subject.dock_bike(double(:bike)) }
+      expect { subject.dock_bike(double(:bike)) }.to raise_error 'Docking station full'
     end
   end
 
   it "releases working bikes" do
-    bike = Bike.new
+    bike = double(:bike)
     subject.dock_bike(bike)
     expect(subject.release_bike).to be_working
   end
 
   it "won't release a broken bike" do
-    bike = Bike.new
+    bike = double(:bike)
     bike.report_broken
     subject.dock_bike(bike)
     expect {subject.release_bike}.to raise_error 'All bikes are broken!!!'
   end
 
   it "will release a working bike if broken bikes in the docking station" do
-      subject.dock_bike(Bike.new)
-      broken_bike = Bike.new
+      subject.dock_bike(double(:bike))
+      broken_bike = double(:bike)
       broken_bike.report_broken
       subject.dock_bike(broken_bike)
       expect(subject.release_bike).to be_working
@@ -43,17 +43,20 @@ end
   it { is_expected.to respond_to(:dock_bike).with(1).argument}
   it { is_expected.to respond_to :bike}
 
+  ### THESE TESTS CHECK NIL AGAINST NIL, NEED FIXING ###
   it 'docks something' do
-    bike = Bike.new
+    bike = double(:bike)
     subject.dock_bike(bike)
     expect(subject.bike).to eq @bikes
   end
 
   it 'returns docked bike' do
-    bike = Bike.new
+    bike = double(:bike)
     subject.dock_bike(bike)
+    p subject.bike
     expect(subject.bike).to eq @bikes
   end
+  ######################################################
 
   it 'returns docking station capacity' do
     dockingstation = DockingStation.new
